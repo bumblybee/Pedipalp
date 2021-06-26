d216d85f0cb3257660c7d9f501d03be7fed6f004
Tiffani Hess <hesstjune@gmail.com>
2021-06-26 14:08:24 -0500

    Home styling and class

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,30 @@
 import React, { useEffect, useState } from "react";
 import { getSpiders } from "../api/spiderApi";
 import HomeCard from "../components/cards/HomeCard";
-import Box from "@material-ui/core/Box";
+import { makeStyles } from "@material-ui/core/styles";
 import Skeleton from "@material-ui/lab/Skeleton";
 
-const Home = () => {
-  const [spiders, setSpiders] = useState([]);
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "50%",
+    height: "100%",
+    margin: "5% auto",
+    "@media(max-width: 640px)": {
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "space-between",
+      width: "100%",
+    },
+  },
+}));
 
+const Home = () => {
+  const classes = useStyles();
+  const [spiders, setSpiders] = useState([]);
   const fetchSpiders = async () => {
     const res = await getSpiders();
     console.log(res);
@@ -18,33 +36,18 @@ const Home = () => {
   }, []);
 
   return spiders && spiders.length ? (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        width: "100%",
-      }}
-    >
+    <div className={classes.root}>
       {spiders.map((spider) => (
         <HomeCard spider={spider} />
       ))}
     </div>
   ) : (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Skeleton variant="rect" width={310} height={240} animation="false" />
+    <div className={classes.root}>
+      <Skeleton variant="rect" width={310} height={240} />
       <br />
-      <Skeleton variant="rect" width={310} height={240} animation="false" />
+      <Skeleton variant="rect" width={310} height={240} />
       <br />
-      <Skeleton variant="rect" width={310} height={240} animation="false" />
+      <Skeleton variant="rect" width={310} height={240} />
     </div>
   );
 };
