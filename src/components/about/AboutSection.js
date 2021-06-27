bef933e27cc5e58876f99cbc4c16973a3c93f0c0
Tiffani Hess <hesstjune@gmail.com>
2021-06-27 09:29:45 -0500

    About styling

--- src/components/about/AboutSection.js
+++ src/components/about/AboutSection.js
@@ -1,11 +1,26 @@
 import React from "react";
-import Container from "@material-ui/core/Container";
 import { makeStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import BugReportIcon from "@material-ui/icons/BugReport";
+import ScheduleIcon from "@material-ui/icons/Schedule";
+import NotesIcon from "@material-ui/icons/Notes";
+import Divider from "@material-ui/core/Divider";
+import {
+  lime,
+  cyan,
+  lightBlue,
+  lightGreen,
+  pink,
+  red,
+} from "@material-ui/core/colors";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     height: "35vh",
-    width: "100%",
+    width: "95%",
   },
   text: {
     textAlign: "left",
@@ -15,23 +30,43 @@ const useStyles = makeStyles((theme) => ({
   title: {
     marginRight: "1rem",
   },
+  bug: {
+    color: lightGreen["A700"],
+  },
+  clock: {
+    color: lightBlue["A700"],
+  },
+  about: {
+    color: pink["A700"],
+  },
 }));
 
 const AboutSection = ({ spider }) => {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <div className={classes.text}>
-        <h4 className={classes.title}>Species:</h4> <p>{spider.species}</p>
-      </div>
-      <div className={classes.text}>
-        <h4 className={classes.title}>Age:</h4> <p>{spider.age}</p>
-      </div>
-      <div className={classes.text}>
-        <h4 className={classes.title}>About:</h4> <p>{spider.about}</p>
-      </div>
-    </div>
+    <List className={classes.root}>
+      <ListItem>
+        <ListItemIcon>
+          <BugReportIcon className={classes.bug} />
+        </ListItemIcon>
+        <ListItemText primary="Species" secondary={spider.species} />
+      </ListItem>
+      <Divider variant="inset" component="li" />
+      <ListItem>
+        <ListItemIcon>
+          <ScheduleIcon className={classes.clock} />
+        </ListItemIcon>
+        <ListItemText primary="Age" secondary={spider.age} />
+      </ListItem>
+      <Divider variant="inset" component="li" />
+      <ListItem>
+        <ListItemIcon>
+          <NotesIcon className={classes.about} />
+        </ListItemIcon>
+        <ListItemText primary="About" secondary={spider.about} />
+      </ListItem>
+    </List>
   );
 };
 
